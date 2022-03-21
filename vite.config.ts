758fae52efae1a07d612990067c9d97baa8bdf92
d kloimwieder <dkkloimwieder@gmail.com>
2022-03-20 21:51:17 -0400

    adjust

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import styles from "rollup-plugin-styles";
+import eslint from "vite-plugin-eslint";
+
+// import styles from "rollup-plugin-styles";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), eslint()],
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        unsafe: true,
+        unsafe_arrows: true,
+        unsafe_Function: true,
+        unsafe_math: true,
+        unsafe_symbols: true,
+        unsafe_methods: true,
+        unsafe_proto: true,
+        unsafe_regexp: true,
+        passes: 3,
+      },
+    },
+
     // rollupOptions: {
     //   plugins: [styles()],
     // },
