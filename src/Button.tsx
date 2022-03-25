bfeaff27d4dd45956d2c7bafd38f4f09402ca0bc
d kloimwieder <dkkloimwieder@gmail.com>
2022-03-24 21:08:53 -0400

    children

---
+++ src/Button.tsx
@@ -0,0 +1,29 @@
+import Slot from './Slot'
+
+
+const Display = (props) => {
+    if (props.color) {
+        return (
+            <div className="display" style={{ color: props.color }}>
+                {props.children}
+            </div>
+        )
+    } else {
+        return (
+            <div className="display">
+                {props.children}
+            </div>
+        )
+    }
+}
+const Button = () => {
+    return(
+        <Slot color='red'>
+            <Display stlye={{color:'blue'}}>Hello</Display>
+            <Display>World</Display>
+           
+        </Slot>
+    )
+}
+
+export default Button
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import { render } from "solid-js/web";
 //import "./index.css";
 //import App from "./App";
 //import Bapp from "./Bapp";
-import Blocks from "./Blocks";
+//import Blocks from "./Blocks";
+import Button from './Button';
 import "./blocks.css";
 
-render(() => <Blocks />, document.getElementById("root") as HTMLElement);
+
+render(() => <Button />, document.getElementById("root") as HTMLElement);
